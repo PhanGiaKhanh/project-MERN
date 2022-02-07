2d0713da447b7b32046dc4300d4b7ab4df1d385a
unknown <phangiakhanh90@gmail.com>
2022-02-07 22:59:46 +0700

    up api/auth/login

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -53,5 +53,47 @@ router.post("/register", async (req, res) => {
 // @route POST api/auth/login
 // @desc Register user
 // @access Public
-router.post("/login", async (req, res) => {});
+router.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+
+  // Simple validation
+  if (!username || !password) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Missing username and/or password" });
+  }
+
+  try {
+    // Check for existing user
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Incorrect username or password" });
+    }
+
+    // Username found
+    const passwordValid = await argon2.verify(user.password, password);
+    if (!passwordValid) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Incorrect username or password" });
+    }
+
+    // Password valid
+    // Return token
+    const accessToken = jwt.sign(
+      { userId: user._id },
+      process.env.ACCESS_TOKEN_SECRET
+    );
+    res.json({
+      success: true,
+      message: "User logged successfully",
+      accessToken,
+    });
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({ success: false, message: "Internal server error" });
+  }
+});
 module.exports = router;
