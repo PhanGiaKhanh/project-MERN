7ab0ca396d29e7f5e6f3e90c2577912bfaefdba4
unknown <phangiakhanh90@gmail.com>
2022-02-08 11:53:54 +0700

    up client

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Landing from "./components/layout/Landing";
+import Login from "./components/auth/Login";
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
