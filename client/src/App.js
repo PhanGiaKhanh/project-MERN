45b79e040cbae8ffd5e0bffb843a3a4fbb4f0034
unknown <phangiakhanh90@gmail.com>
2022-02-08 21:43:47 +0700

    up react Register and Login with Login connect server

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,22 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Landing from "./components/layout/Landing";
-import Login from "./components/auth/Login";
+import Auth from "./views/Auth";
+import AuthContextProvider from "./contexts/AuthContext";
+import Dashboard from "./views/Dashboard";
+
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </Router>
+    <AuthContextProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/login" element={<Auth authRoute="login" />} />
+          <Route path="/register" element={<Auth authRoute="register" />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </Router>
+    </AuthContextProvider>
   );
 }
 
---
+++ client/src/components/auth/LoginForm.js
@@ -0,0 +1,77 @@
+import React from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import { Link, useNavigate } from "react-router-dom";
+import { useState, useContext } from "react";
+import { AuthContext } from "../../contexts/AuthContext";
+
+const LoginForm = () => {
+  // contexts
+  const { loginUser } = useContext(AuthContext);
+
+  // Router
+  const navigate = useNavigate();
+
+  // Local state
+  const [loginForm, setLoginForm] = useState({
+    username: "",
+    password: "",
+  });
+  const { username, password } = loginForm;
+  const onChangeLoginForm = (event) =>
+    setLoginForm({ ...loginForm, [event.target.name]: event.target.value });
+
+  const login = async (event) => {
+    // Tránh submit theo form HTML
+    event.preventDefault();
+
+    try {
+      const loginData = await loginUser(loginForm);
+      if (loginData.success) {
+        navigate("/dashboard");
+      } else {
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <>
+      <Form className="my-4" onSubmit={login}>
+        <Form.Group>
+          <Form.Control
+            type="text"
+            placeholder="Username"
+            name="username"
+            required
+            value={username}
+            onChange={onChangeLoginForm}
+          ></Form.Control>
+        </Form.Group>
+        <Form.Group className="mt-2">
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            name="password"
+            required
+            value={password}
+            onChange={onChangeLoginForm}
+          ></Form.Control>
+        </Form.Group>
+        <Button variant="success" type="submit" className="mt-3">
+          Login
+        </Button>
+      </Form>
+      <p>
+        Don't have an account?
+        <Link to="/register">
+          <Button variant="info" size="sm" className="ml-2">
+            Register
+          </Button>
+        </Link>
+      </p>
+    </>
+  );
+};
+
+export default LoginForm;
